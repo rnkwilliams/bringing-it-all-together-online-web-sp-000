a742647f3035da5555cf5a9704f6c43e20c8872d
Ranika Williams <rnkprice07@gmail.com>
2019-08-14 15:30:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,7 +57,7 @@ class Dog
     sql = <<-SQL
     SELECT * FROM dogs
     WHERE id = ?
-    LIMIT = 1
+    LIMIT 1
     SQL
     
     DB[:conn].execute(sql, id).map do |row|
@@ -69,7 +69,7 @@ class Dog
     sql = <<-SQL
     SELECT * FROM dogs
     WHERE name = ?
-    LIMIT = 1
+    LIMIT 1
     SQL
     
     DB[:conn].execute(sql, name).map do |row|
