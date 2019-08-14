4955e8c7becd6de4fd122c7c6a3896214e1e54e4
Ranika Williams <rnkprice07@gmail.com>
2019-08-14 15:18:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,6 +53,10 @@ class Dog
     self.new(id: id, name: name, breed: breed)
   end
   
+  def self.find_by_id(id)
+    
+  end
+  
   def self.find_by_name(name)
     sql = <<-SQL
     SELECT * FROM dogs
